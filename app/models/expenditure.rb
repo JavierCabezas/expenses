fe237efe9895fad2e8104a3023ad9c409f6d46b1
Javier Cabezas <jcleyton@gmail.com>
2015-11-15 12:59:22 -0300

    Used partials to separate the sections on the expenditures index view

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -7,13 +7,21 @@ class Expenditure < ActiveRecord::Base
     ['Overdue', 'Payed', 'Invalid']
   end
 
-  def self.array_chart
+  #@todo: Make these two functions not suck.
+  def self.array_chart_expense_type
     out = Hash.new
-
     arr = Expenditure.group(:expense_type_id).sum(:ammount).map{ |expe|
       out[ ExpenseType.find_by_id(expe[0]).name] = expe[1]
     }
-
     out
   end
+
+  def self.array_chart_expense_by_user
+    out = Hash.new
+    arr = Expenditure.group(:user_id).sum(:ammount).map{ |expe|
+      out[ User.find_by_id(expe[0]).username] = expe[1]
+    }
+    out
+  end
+
 end
