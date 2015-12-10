01e0b2ecb10cc9ca5a6ed3973b12fff3267173fa
Javier Cabezas <jcleyton@gmail.com>
2015-12-10 19:57:53 -0300

    Showing all the users on the is user in house? section

--- app/controllers/is_user_in_house_controller_controller.rb
+++ app/controllers/is_user_in_house_controller_controller.rb
@@ -1,5 +1,13 @@
 class IsUserInHouseControllerController < ApplicationController
   def index
-    @is = IsUserInHouse.order('user_id DESC, month_id ASC, day ASC').all
+
+    if params['month_id'].present?
+      @selected_month_id = params['month_id']
+    else
+      @selected_month_id = Month.get_current_month_id
+    end
+
+    @was_at_home = IsUserInHouse.where(month_id: @selected_month_id).order(:day).all.to_a
+    @users = User.all
   end
 end
